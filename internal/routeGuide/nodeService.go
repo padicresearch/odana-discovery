299ae4bbfee12e26608ae6224ab7ff0edc7717e2
Johnson Osei Yeboah <johnyoat@gmail.com>
2023-01-26 18:07:36 -0600

    feat:basic impmenetations of the TelemetryService

--- internal/routeGuide/nodeService.go
+++ internal/routeGuide/nodeService.go
@@ -1,1 +1,12 @@
 package routeGuide
+
+import (
+	"fmt"
+	"github.com/padicresearch/odana-discovery/internal/proto"
+	"golang.org/x/net/context"
+)
+
+func (s *Server) FindClosestPeers(ctx context.Context, request *proto.FindClosestPeersRequest) (*proto.FindClosestPeersResponse, error) {
+	fmt.Println("Find Closet Peers")
+	return nil, nil
+}
--- internal/routeGuide/statusService.go
+++ internal/routeGuide/statusService.go
@@ -1,1 +1,13 @@
 package routeGuide
+
+import (
+	"fmt"
+	"github.com/padicresearch/odana-discovery/internal/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) Status(ctx context.Context, status *proto.NodeStatus) (*emptypb.Empty, error) {
+	fmt.Println("Node Status")
+	return nil, nil
+}
